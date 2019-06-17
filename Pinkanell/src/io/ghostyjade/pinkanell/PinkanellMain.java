68ce0ab64172259d4f3fb393b4f06d3ff04b18e9
Andrea-C1201 <giandreas@outlook.it>
2019-06-17 10:01:17 +0200

    Added PallettahRecognition project and some useless things.

---
+++ Pinkanell/src/io/ghostyjade/pinkanell/PinkanellMain.java
@@ -0,0 +1,13 @@
+package io.ghostyjade.pinkanell;
+
+public class PinkanellMain {
+
+	public PinkanellMain() {
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+	// Add EventQueue.invokeLater() to create multithreaded ;
+}
