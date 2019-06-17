c4986fb25a0ac0fa36e0b2457bef2eea4e640e58
Michele Tagliani <giantaglio@gmail.com>
2019-06-17 17:17:08 +0200

    UI with a test window

--- Pinkanell/src/io/taglioiscoding/ui/MainWindow.java
+++ Pinkanell/src/io/taglioiscoding/ui/MainWindow.java
@@ -1,5 +1,6 @@
 package io.taglioiscoding.ui;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Toolkit;
 
@@ -7,7 +8,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import java.awt.Color;
 import javax.swing.border.LineBorder;
 
 /**
@@ -19,6 +19,12 @@ import javax.swing.border.LineBorder;
 public class MainWindow {
 
 	private JFrame frame;
+	private JPanel panel;
+	private JLabel lblVmax;
+	private JLabel valueVmed;
+	private JLabel lblVmed, valueVmax, Player1, Player2, points1, points2, scoreDivisor;
+	
+	
 
 	/**
 	 * Create the application.
@@ -38,47 +44,47 @@ public class MainWindow {
 		frame = new JFrame();
 		frame.getContentPane().setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		frame.setResizable(false);
-		frame.setUndecorated(true);
+		//XXX frame.setUndecorated(true);
 		frame.getContentPane().setBackground(new Color(238, 238, 238));
 		frame.setBackground(Color.WHITE);
 		frame.setBounds(0, 0, (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(),
 				(int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setBorder(new LineBorder(Color.ORANGE, 3));
 		panel.setBounds(6, 30, 826, 592);
 
-		JLabel lblVmax = new JLabel("Velocità massima:");
+		lblVmax = new JLabel("Velocità massima:");
 		lblVmax.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		lblVmax.setBounds(844, 93, 358, 72);
 
-		JLabel valueVmed = new JLabel("000.00");
+		valueVmed = new JLabel("000.00");
 		valueVmed.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		valueVmed.setBounds(1228, 189, 193, 96);
 
-		JLabel lblVmed = new JLabel("Velocità media:");
+		lblVmed = new JLabel("Velocità media:");
 		lblVmed.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		lblVmed.setBounds(844, 206, 358, 62);
 
-		JLabel valueVmax = new JLabel("000.00");
+		valueVmax = new JLabel("000.00");
 		valueVmax.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		valueVmax.setBounds(1228, 93, 185, 72);
 
-		JLabel Player1 = new JLabel("Player 1");
+		Player1 = new JLabel("Player 1");
 		Player1.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		Player1.setBounds(431, 652, 236, 41);
 
-		JLabel Player2 = new JLabel("Player 2");
+		Player2 = new JLabel("Player 2");
 		Player2.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		Player2.setBounds(759, 641, 241, 62);
 
-		JLabel points1 = new JLabel("0");
+		points1 = new JLabel("0");
 		points1.setBounds(460, 652, 104, 197);
 		points1.setHorizontalAlignment(SwingConstants.CENTER);
 		points1.setFont(new Font("Lucida Grande", Font.PLAIN, 99));
 
-		JLabel points2 = new JLabel("0");
+		points2 = new JLabel("0");
 		points2.setBounds(731, 630, 206, 236);
 		points2.setHorizontalAlignment(SwingConstants.CENTER);
 		points2.setFont(new Font("Lucida Grande", Font.PLAIN, 99));
@@ -94,13 +100,55 @@ public class MainWindow {
 		frame.getContentPane().add(points1);
 		frame.getContentPane().add(points2);
 
-		JLabel scoreDivisor = new JLabel(":");
+		scoreDivisor = new JLabel(":");
 		scoreDivisor.setHorizontalAlignment(SwingConstants.CENTER);
 		scoreDivisor.setFont(new Font("Lucida Grande", Font.BOLD, 99));
 		scoreDivisor.setBounds(657, 702, 73, 96);
 		frame.getContentPane().add(scoreDivisor);
 	}
 
+	public void changeToCameraPreview() {
+		panel.setBorder(new LineBorder(Color.ORANGE, 3));
+		panel.setBounds(0, 0,((Toolkit.getDefaultToolkit().getScreenSize().width) /2 - 10),
+				640);
+
+		lblVmax.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+		lblVmax.setBounds(61, 654, 358, 72);
+
+		valueVmed.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+		valueVmed.setBounds(445, 750, 193, 96);
+
+		lblVmed.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+		lblVmed.setBounds(61, 767, 358, 62);
+
+		valueVmax.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+		valueVmax.setBounds(445, 654, 185, 72);
+
+		Player1.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+		Player1.setBounds(797, 680, 236, 41);
+
+		Player2.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+		Player2.setBounds(1125, 669, 241, 62);
+
+		points1.setBounds(826, 680, 104, 197);
+		points1.setHorizontalAlignment(SwingConstants.CENTER);
+		points1.setFont(new Font("Lucida Grande", Font.PLAIN, 99));
+
+		points2.setBounds(1097, 658, 206, 236);
+		points2.setHorizontalAlignment(SwingConstants.CENTER);
+		points2.setFont(new Font("Lucida Grande", Font.PLAIN, 99));
+
+		scoreDivisor.setHorizontalAlignment(SwingConstants.CENTER);
+		scoreDivisor.setFont(new Font("Lucida Grande", Font.BOLD, 99));
+		scoreDivisor.setBounds(1023, 730, 73, 96);
+		
+		JPanel cameraPanel = new JPanel();
+		cameraPanel.setBorder(new LineBorder(new Color(0, 255, 0), 3));
+		cameraPanel.setBounds(panel.getWidth(), 0 ,((Toolkit.getDefaultToolkit().getScreenSize().width) /2 - 10),
+				 640);
+		frame.getContentPane().add(cameraPanel);
+	}
+	
 	/**
 	 * Show the window.
 	 * 
