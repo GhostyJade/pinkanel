0c70f9843fa23e549f65a3f3e1a9a67a7d929ca1
GhostyJade <giandreas@outlook.it>
2019-06-19 10:27:12 +0200

    Added some docs

--- Pinkanell/src/io/ghostyjade/utils/Constants.java
+++ Pinkanell/src/io/ghostyjade/utils/Constants.java
@@ -1,12 +1,29 @@
 package io.ghostyjade.utils;
 
+/**
+ * This class hold some constants values.
+ * 
+ * @author GhostyJade
+ */
 public class Constants {
 
+	/**
+	 * The field width
+	 */
 	public static int FIELD_WIDHT;
+	/**
+	 * The field height
+	 */
 	public static int FIELD_HEIGHT;
 
+	/**
+	 * The locale name
+	 */
 	public static String LOCALE_NAME;
 
+	/**
+	 * Initializes the fields with some values.
+	 */
 	static {
 		FIELD_WIDHT = 0;
 		FIELD_HEIGHT = 0;
